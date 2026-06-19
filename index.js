0cc21bfef2861d81599a9023e096735e851b9d7a
Ислам Кучменов <islamkuchmenov@MacBook-Air-Islam.local>
2026-06-19 21:39:38 +0300

    fix: use dynamic PORT for Railway

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const FIREBASE_API_KEY = 'AIzaSyD2V5E2i9LvMr6fw9uRYZQlOA1BYKNJhmc';
 
 app.use(cors());
